ce50efa16919f5049699244b08b7bd8a3d36f1b2
JW <ronbyronn@gmail.com>
2023-11-05 17:16:11 +0900

    3.1 DefinitelyTyped

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+	background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% { 
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.div`
+	font-size: 80px;
+`;
+
+const Box = styled.div`
+	height: 200px;
+	width: 200px;
+	justify-content: center;
+	align-items: center;
+	background-color: ${(props) => props.theme.textColor};
+	animation: ${rotation} linear infinite 2s;
+	display: flex;
+	${Emoji}:hover {
+		font-size: 160px;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Box>
+				<Emoji as="p">😎</Emoji>
+			</Box>
+		</Wrapper>
+	);
 }
 
 export default App;
