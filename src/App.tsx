01e9e2d44e5c236c63f2dc4b273b1a8fdec6eaa8
JW <ronbyronn@gmail.com>
2023-11-05 21:31:32 +0900

    3.2 Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,10 @@
-import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-	display: flex;
-	width: 100vw;
-	height: 100vh;
-	justify-content: center;
-	align-items: center;
-	background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const rotation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% { 
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.div`
-	font-size: 80px;
-`;
-
-const Box = styled.div`
-	height: 200px;
-	width: 200px;
-	justify-content: center;
-	align-items: center;
-	background-color: ${(props) => props.theme.textColor};
-	animation: ${rotation} linear infinite 2s;
-	display: flex;
-	${Emoji}:hover {
-		font-size: 160px;
-	}
-`;
+import Circle from "./Circle";
 
 function App() {
 	return (
-		<Wrapper>
-			<Box>
-				<Emoji as="p">😎</Emoji>
-			</Box>
-		</Wrapper>
+		<div>
+			<Circle bgColor="teal" />
+		</div>
 	);
 }
 
