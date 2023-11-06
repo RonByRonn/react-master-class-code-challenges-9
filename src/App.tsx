a8ac4e463dc347615368dd0ace4bea9ff539b655
JW <ronbyronn@gmail.com>
2023-11-07 01:07:47 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+	const [value, setValue] = useState("");
+	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+		console.log(event.currentTarget.value);
+		setValue(event.currentTarget.value);
+	};
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		console.log(event);
+		event.preventDefault();
+		setValue("");
+	};
 	return (
 		<div>
-			<Circle borderColor="yellow" bgColor="teal" />
-			<Circle text="i am here" bgColor="tomato" />
+			<form onSubmit={onSubmit}>
+				<input
+					value={value}
+					placeholder="Username"
+					type="text"
+					onChange={onChange}
+				/>
+				<button>Submit</button>
+			</form>
 		</div>
 	);
 }
