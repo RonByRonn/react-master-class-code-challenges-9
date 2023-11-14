5039df0f9103332482d822c5860f7f0d1a2db66a
JW <ronbyronn@gmail.com>
2023-11-14 17:42:11 +0900

    6.6 Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-
-function ToDoList() {
+/* function ToDoList() {
 	const [toDo, setToDo] = useState("");
+	const [toDoError, setToDoError] = useState("");
 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
 		const {
 			currentTarget: { value },
 		} = event;
-
+		setToDoError("");
 		setToDo(value);
 	};
 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log(toDo);
+		if (toDo.length < 10) {
+			return setToDoError("To do should be longer");
+		}
 		setToDo("");
 	};
 
@@ -19,6 +20,23 @@ function ToDoList() {
 		<div>
 			<form onSubmit={onSubmit}>
 				<input onChange={onChange} value={toDo} placeholder="Write a to-do" />
+				<button>Add</button>
+				{toDoError !== "" ? toDoError : null}
+			</form>
+		</div>
+	);
+} */
+
+import { useForm } from "react-hook-form";
+
+function ToDoList() {
+	const { register, watch } = useForm();
+	console.log(watch());
+
+	return (
+		<div>
+			<form>
+				<input {...register("toDo")} placeholder="Write a to-do" />
 				<button>Add</button>
 			</form>
 		</div>
