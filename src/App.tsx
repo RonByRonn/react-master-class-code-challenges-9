6b3dada87074a7d89b3147e8de38f55968d7461b
JW <ronbyronn@gmail.com>
2023-11-15 18:29:37 +0900

    7.0 Get Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-import ToDoList from "./components/ToDoList";
-
-const GlobalStyle = createGlobalStyle`
-  	@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400&display=swap');
-	${reset}
-	* {
-		box-sizing: border-box;
-	}
-	body {
-		font-family: 'Source Code Pro', monospace;
-		background-color: ${(props) => props.theme.bgColor};
-		color: ${(props) => props.theme.textColor};
-		font-weight: 300;
-		line-height: 1.2;
-	}
-	a {
-		text-decoration: none;
-		color: inherit;
-	}
-`;
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hoursSelector, minutesState } from "./atoms";
 
 function App() {
+	const [minutes, setMinutes] = useRecoilState(minutesState);
+	const hours = useRecoilValue(hoursSelector);
+	const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+		// Convert string to number: +
+		setMinutes(event.currentTarget.value);
+	};
 	return (
-		<>
-			<GlobalStyle />
-			<ToDoList />
-		</>
+		<div>
+			<input
+				value={minutes}
+				onChange={onMinutesChange}
+				type="number"
+				placeholder="Minutes"
+			/>
+			<input value={hours} type="number" placeholder="Hours" />
+		</div>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import App from "./App";
 import { darkTheme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  	@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400&display=swap');
+	${reset}
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		font-family: 'Source Code Pro', monospace;
+		background-color: ${(props) => props.theme.bgColor};
+		color: ${(props) => props.theme.textColor};
+		font-weight: 300;
+		line-height: 1.2;
+	}
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+`;
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -12,6 +32,7 @@ root.render(
 	<React.StrictMode>
 		<RecoilRoot>
 			<ThemeProvider theme={darkTheme}>
+				<GlobalStyle />
 				<App />
 			</ThemeProvider>
 		</RecoilRoot>
