226c962fb17f20ab4ba173d83455ddddfdec27cf
JW <ronbyronn@gmail.com>
2023-11-15 13:23:52 +0900

    6.14 Immutability part One

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+	const setToDos = useSetRecoilState(toDoState);
+	const onClick = (newCategory: IToDo["category"]) => {};
+	const onClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const {
+			currentTarget: { name },
+		} = event;
+
+		setToDos((oldToDos) => {
+			const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+			const oldToDo = oldToDos[targetIndex];
+			const newToDo = { text, id, category: name };
+		});
+	};
 	return (
 		<li>
 			<span>{text}</span>
-			<button>Doing</button>
-			<button>To Do</button>
-			<button>Done</button>
+			{category !== "DOING" && (
+				// <button onClick={() => onClick("DOING")}>Doing</button>
+				<button name="DOING" onClick={onClick2}>
+					Doing
+				</button>
+			)}
+			{category !== "TO_DO" && (
+				// <button onClick={() => onClick("TO_DO")}>To Do</button>
+				<button name="TO_DO" onClick={onClick2}>
+					To Do
+				</button>
+			)}
+			{category !== "DONE" && (
+				// <button onClick={() => onClick("DONE")}>Done</button>
+				<button name="DONE" onClick={onClick2}>
+					Done
+				</button>
+			)}
 		</li>
 	);
 }
