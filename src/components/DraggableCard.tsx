b6740c751ca1363276bbc6b81c57a9ef750abc75
JW <ronbyronn@gmail.com>
2023-11-16 20:46:24 +0900

    7.12 Final Styles

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
 	border-radius: 5px;
 	padding: 10px 10px;
-	background-color: ${(props) => props.theme.cardColor};
+	background-color: ${(props) =>
+		props.isDragging ? "#74b9ff" : props.theme.cardColor};
 	margin-bottom: 5px;
+	box-shadow: ${(props) =>
+		props.isDragging ? "0px 2px 5px rgba(0,0,0,0.1)" : "none"};
+	// 위 5px 부분은 blur 관련된 부분임.
 `;
 
 interface IDraggableCardProps {
@@ -18,8 +22,9 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
 	return (
 		// key와 draggableId가 같아야 함 (이 패키지 특징)
 		<Draggable key={toDo} draggableId={toDo} index={index}>
-			{(magic) => (
+			{(magic, snapshot) => (
 				<Card
+					isDragging={snapshot.isDragging}
 					ref={magic.innerRef}
 					{...magic.draggableProps}
 					{...magic.dragHandleProps}
