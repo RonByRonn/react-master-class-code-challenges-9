e59b13e9d8fdf6c698269036ec3cc52223616cb6
JW <ronbyronn@gmail.com>
2023-11-18 14:11:24 +0900

    8.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -9,20 +10,22 @@ const Wrapper = styled.div`
 	align-items: center;
 `;
 
+const BiggerBox = styled.div`
+	width: 600px;
+	height: 600px;
+	background-color: rgba(255, 255, 255, 0.2);
+	border-radius: 40px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	/* overflow: hidden; */
+`;
+
 const Box = styled(motion.div)`
 	width: 200px;
 	height: 200px;
 	background-color: rgba(255, 255, 255, 1);
 	border-radius: 40px;
-	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-	background-color: white;
-	height: 70px;
-	width: 70px;
-	place-self: center;
-	border-radius: 35px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -33,16 +36,24 @@ const boxVariants = {
 };
 
 function App() {
+	const biggerBoxRef = useRef<HTMLDivElement>(null);
 	return (
 		<Wrapper>
 			{/* 아래 whileDrag의 백그라운드 컬러에 그냥 "블루" 같은 스트링을 적으면 색상 변화에 애니메이션이 없음. rgb나 rgba로 숫자를 적어야 함 */}
-			<Box
-				drag
-				variants={boxVariants}
-				whileHover="hover"
-				whileDrag="drag"
-				whileTap="click"
-			/>
+			<BiggerBox ref={biggerBoxRef}>
+				<Box
+					// drag="x"
+					drag
+					// dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }}
+					dragConstraints={biggerBoxRef}
+					dragSnapToOrigin
+					dragElastic={0.5}
+					variants={boxVariants}
+					whileHover="hover"
+					whileDrag="drag"
+					whileTap="click"
+				/>
+			</BiggerBox>
 		</Wrapper>
 	);
 }
